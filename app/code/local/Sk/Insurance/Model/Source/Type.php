501cfe0faf721cca878bb0a738a8fbba29d5c36e
SkvokeN <skvooka@gmail.com>
2017-08-09 10:37:05 +0300

    add function toArray() in Source_Type

--- app/code/local/Sk/Insurance/Model/Source/Type.php
+++ app/code/local/Sk/Insurance/Model/Source/Type.php
@@ -3,11 +3,29 @@
 
 class Sk_Insurance_Model_Source_Type
 {
+    /**
+     * Options getter
+     *
+     * @return array
+     */
     public function toOptionArray()
     {
         return array(
             array('value' => 1, 'label' => 'Absolute'),
             array('value' => 2, 'label' => 'Percentage'),
+        );
+    }
+
+    /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return array(
+            1 => 'Absolute',
+            2 => 'Percentage',
         );
     }
 }
